aa6ff3020c08479fe54fb68c6eec90bcaf57ce1f
Tim-PF <tim.pfops@gmail.com>
2023-09-13 08:16:21 +0200

    Grid will now adapt to wished size
    Added logic to code that change FlexBasis to change the size of each
    pixel and make it fit into the sketch div.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,11 +1,23 @@
-let gridSize = 16;
+let gridSize = 10;
 grid = gridSize * gridSize;
+let flexBasisCalculator = 100 / gridSize;
 
 const mainContainer = document.querySelector(".sketch-container");
 for(let i= 0; i<grid; i++) {
    
    const container = document.createElement('div');
    container.classList.add('small-grid');
+   const flexBasisValue = `${flexBasisCalculator}%`;
+   container.style.flexBasis = flexBasisValue;
    mainContainer.appendChild(container);
 
 }
+
+
+// const gridDiv = document.querySelectorAll('.small-grid');
+
+// gridDiv.forEach(item => {
+//    const flexBasisValue = `${flexBasisCalculator}%`;
+   
+//    item.style.flexBasis = flexBasisValue;
+//});
