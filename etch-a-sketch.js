6a46bef8be782c54cc3185707613d358ef0286c9
Tim-PF <tim.pfops@gmail.com>
2023-09-13 10:48:59 +0200

    Added Slider Grid now change by slider value

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -13,11 +13,34 @@ for(let i= 0; i<grid; i++) {
 
 }
 
+const slider = document.querySelector('#slider');
+const sliderValue = document.querySelector('#sliderValue')
+sliderValue.textContent = `${slider.value} x ${slider.value} `;
+console.log(typeof(slider.value));
+slider.addEventListener("input", () => {
+   sliderValue.textContent = `${slider.value} x ${slider.value} `;
+   updateGrid();
+});
 
-// const gridDiv = document.querySelectorAll('.small-grid');
 
-// gridDiv.forEach(item => {
-//    const flexBasisValue = `${flexBasisCalculator}%`;
-   
-//    item.style.flexBasis = flexBasisValue;
-//});
+
+function updateGrid() {
+   gridSize = slider.value;
+   grid = gridSize * gridSize;
+   flexBasisCalculator = 100 / gridSize;
+
+   const gridDiv = document.querySelectorAll('.small-grid');
+
+   gridDiv.forEach(item => {
+       item.remove(); // Remove each individual item
+   });
+
+   // Now, recreate the grid elements based on the new values
+   for (let i = 0; i < grid; i++) {
+       const container = document.createElement('div');
+       container.classList.add('small-grid');
+       const flexBasisValue = `${flexBasisCalculator}%`;
+       container.style.flexBasis = flexBasisValue;
+       mainContainer.appendChild(container);
+   }
+}
